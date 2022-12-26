dc2f25ea9576703b46081d6965780203f316a577
Prakashdat <prakashdata@gmail.com>
2022-12-26 12:01:47 +0530

    Add project files.

---
+++ TODAY26.12.22-11.08am/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TODAY26._12._22_11._08am.Models;
+
+namespace TODAY26._12._22_11._08am.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TODAY26.12.22-11.08am/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TODAY26._12._22_11._08am.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
